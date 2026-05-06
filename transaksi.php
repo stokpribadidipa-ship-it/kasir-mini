1258ff1b25b2963854338a15252a752d71499e8f
mandriva <stokpribadidipa@gmail.com>
2026-05-06 12:40:26 +0700

    Update transaksi.php

--- transaksi.php
+++ transaksi.php
@@ -1,13 +1,7 @@
-<?php include 'koneksi.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transaksi</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+<?php 
+include 'koneksi.php'; 
+include 'header.php';
+?>
 
 <div class="container">
 <div class="row justify-content-center">
