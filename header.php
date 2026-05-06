f96ed65a4ac4a9913e5b7fb43ea3d6c3bd1894cd
mandriva <stokpribadidipa@gmail.com>
2026-05-06 12:45:59 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container">
+
+    <a class="navbar-brand" href="kasir.php">Kasir Mini</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menu">
+        <ul class="navbar-nav ms-auto">
+
+            <li class="nav-item">
+                <a class="nav-link" href="admin.php">Beranda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="kasir.php">Kasir</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="transaksi.php">Transaksi</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="tampil_transaksi.php">Riwayat</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="barang.php">Barang</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="tampil_barang.php">Data Barang</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link text-danger" href="logout.php">Logout</a>
+            </li>
+
+        </ul>
+    </div>
+
+</div>
+</nav>
