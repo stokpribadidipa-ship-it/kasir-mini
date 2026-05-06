78ca8129e48572abb8de8b13307d0158a0c381c5
mandriva <stokpribadidipa@gmail.com>
2026-05-06 13:16:39 +0700

    Update tampil_transaksi.php

--- tampil_transaksi.php
+++ tampil_transaksi.php
@@ -1,33 +1,66 @@
-<?php include "koneksi.php"; 
+<?php 
+include "koneksi.php";
 include "header.php";
+
+// JOIN ke tabel barang
+$data = mysqli_query($conn, "
+    SELECT transaksi.*, barang.nama_barang 
+    FROM transaksi 
+    LEFT JOIN barang ON transaksi.id_barang = barang.id_barang
+");
+
+// cek error query
+if (!$data) {
+    die("Query Error: " . mysqli_error($conn));
+}
 ?>
 
 <div class="container py-4">
-  <h5 class="mb-3">Data Transaksi</h5>
-  <table class="table table-bordered table-striped">
-    <thead class="table-primary">
-      <tr>
-        <th>#</th>
-        <th>Total</th>
-        <th>Bayar</th>
-        <th>Kembalian</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      $no = 1;
-      $data = mysqli_query($conn, "SELECT * FROM transaksi");
-      while ($d = mysqli_fetch_array($data)):
-      ?>
-      <tr>
-        <td><?= $no++ ?></td>
-        <td>Rp <?= number_format($d['total'], 0, ',', '.') ?></td>
-        <td>Rp <?= number_format($d['bayar'], 0, ',', '.') ?></td>
-        <td>Rp <?= number_format($d['kembalian'], 0, ',', '.') ?></td>
-      </tr>
-      <?php endwhile; ?>
-    </tbody>
-  </table>
+  <h4 class="mb-3">🧾 Data Transaksi</h4>
+
+  <div class="card shadow">
+    <div class="card-body">
+
+      <table class="table table-bordered table-hover">
+        <thead class="table-dark text-center">
+          <tr>
+            <th>No</th>
+            <th>Nama Barang</th>
+            <th>Total</th>
+            <th>Bayar</th>
+            <th>Kembalian</th>
+            <th>Tanggal</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php 
+          $no = 1;
+          while ($d = mysqli_fetch_array($data)) :
+          ?>
+          <tr>
+            <td class="text-center"><?= $no++ ?></td>
+            <td><?= $d['nama_barang'] ?? '-' ?></td>
+            <td class="text-end text-primary">
+              Rp <?= number_format($d['total'], 0, ',', '.') ?>
+            </td>
+            <td class="text-end text-success">
+              Rp <?= number_format($d['bayar'], 0, ',', '.') ?>
+            </td>
+            <td class="text-end text-danger">
+              Rp <?= number_format($d['kembalian'], 0, ',', '.') ?>
+            </td>
+            <td class="text-center">
+              <?= date('d-m-Y H:i', strtotime($d['tanggal'])) ?>
+            </td>
+          </tr>
+          <?php endwhile; ?>
+        </tbody>
+
+      </table>
+
+    </div>
+  </div>
 </div>
 
 </body>
