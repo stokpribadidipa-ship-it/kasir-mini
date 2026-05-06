4d0a2b187a7f83e816f109ef043d017c55574140
mandriva <stokpribadidipa@gmail.com>
2026-05-06 11:26:02 +0700

    Update tampil_transaksi.php

--- tampil_transaksi.php
+++ tampil_transaksi.php
@@ -1,7 +1,40 @@
-<?php
-include "koneksi.php";
-$data = mysqli_query($conn, "SELECT * FROM transaksi");
-while ($d = mysqli_fetch_array($data)) {
-    echo "Total: ".$d['total']." | Bayar: ".$d['bayar']." | Kembalian: ".$d['kembalian']."<br>";
-}
-?>
+<?php include "koneksi.php"; ?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Data Transaksi</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container py-4">
+  <h5 class="mb-3">Data Transaksi</h5>
+  <table class="table table-bordered table-striped">
+    <thead class="table-primary">
+      <tr>
+        <th>#</th>
+        <th>Total</th>
+        <th>Bayar</th>
+        <th>Kembalian</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      $no = 1;
+      $data = mysqli_query($conn, "SELECT * FROM transaksi");
+      while ($d = mysqli_fetch_array($data)):
+      ?>
+      <tr>
+        <td><?= $no++ ?></td>
+        <td>Rp <?= number_format($d['total'], 0, ',', '.') ?></td>
+        <td>Rp <?= number_format($d['bayar'], 0, ',', '.') ?></td>
+        <td>Rp <?= number_format($d['kembalian'], 0, ',', '.') ?></td>
+      </tr>
+      <?php endwhile; ?>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
