69bae7889d6b69de53dd742f2307baee49314c8b
mandriva <stokpribadidipa@gmail.com>
2026-05-06 13:03:38 +0700

    Update proses_transaksi.php

--- proses_transaksi.php
+++ proses_transaksi.php
@@ -5,22 +5,27 @@ function hitungDiskon($harga, $diskon) {
     $potongan = ($diskon / 100) * $harga;
     return $harga - $potongan;
 }
- 
-// Fungsi hitung kembalian
+
 function hitungKembalian($bayar, $total) {
     return $bayar - $total;
 }
 
 if (isset($_POST['harga'])) {
+
+    $barang_id = $_POST['barang_id'];
     $harga = $_POST['harga'];
+    $jumlah = $_POST['jumlah'];
     $diskon = $_POST['diskon'];
     $bayar = $_POST['bayar'];
 
-
-    $total = hitungDiskon($harga, $diskon);
+    $subtotal = $harga * $jumlah;
+    $total = hitungDiskon($subtotal, $diskon);
     $kembalian = hitungKembalian($bayar, $total);
 
-    mysqli_query($conn, "INSERT INTO transaksi VALUES ('', NOW(), '$total', '$bayar', '$kembalian', '1')");
+    mysqli_query($conn, "INSERT INTO transaksi 
+    (barang_id, tanggal, total, bayar, kembalian) 
+    VALUES 
+    ('$barang_id', NOW(), '$total', '$bayar', '$kembalian')");
 
     echo "Total: Rp $total <br>";
     echo "Kembalian: Rp $kembalian";
